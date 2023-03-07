fd6b4238562902f662df68fa4b005476fb5045cb
Noms65 <nomsradnom@gmail.com>
2023-03-07 10:56:01 +0300

    test

---
+++ FrontServlet/src/java/model/Utilitaire.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Utilitaire {
+    public String getProcessUrl(String url){
+        String[] parts = url.split("//");
+        String[] parts2 = parts[1].split("/");
+        
+        return parts2[2];
+        
+    }
+}
